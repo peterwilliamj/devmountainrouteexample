ef9341cc84a24e21db4fbb8a59efecb28a923886
Jake Lingwall <jake.lingwall@domo.com>
2014-10-09 19:40:49 -0600

    promise stuff built on the sample app we wrote together.

--- scripts/pagetwo/pagetwoCtrl.js
+++ scripts/pagetwo/pagetwoCtrl.js
@@ -1,9 +1,38 @@
-angular.module('livecodeApp').controller('pagetwoCtrl', function($scope, $location){
+angular.module('livecodeApp').controller('pagetwoCtrl', function($scope, $location, $timeout, $q, data){
+  // define all your variables at the top of your controller.
   $scope.pageTwoProof = "Good Work";
+  $scope.showingText = false;
 
   $scope.toPageOne = function(){
     $location.path('/pageone')
   }
+
+  $scope.showText = function(){
+    getText().then(function(data){
+      $scope.showingText = data;
+    });
+  }
+
+  function getText(){
+    //$q is angulars promise library.
+    //you must include that at the top when you define your controller.
+    //$q.defer will create a "defer" object which has a .promise property which is a promise;
+    var deffered = $q.defer();
+    //this creates a deferred object with a promise on it.
+    //essentially creating a promise;
+
+    $timeout(function(){
+      //this is how we send data back to whoever requested it from us.
+      //they already have our promise so they know it's going to come.
+      //but we have to resolve this, to actually send the data back to them.
+      deffered.resolve(true);
+    }, 5000);
+
+    //down here we immediatly return the promise back to whoever called us.
+    return deffered.promise;
+  }
+
+ alert(data);
 
 })
 
